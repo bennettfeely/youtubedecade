8454307a1fdb9c589f0dabf3e73e983eaa01c23b
Bennett Feely <bennettfeely@Bennetts-MacBook-Air.local>
2020-02-10 01:48:02 -0500

    hang date on feb 6

--- dist/js/yt.js
+++ dist/js/yt.js
@@ -22,7 +22,7 @@ function debug(date) {
 }
 
 function init() {
-	var targeted_date = moment("2-05-2010");
+	var targeted_date = moment("2-05-2020");
 
 	// Get data
 	loadVideoList(targeted_date.subtract(10, "y"));
